d58b755d49941023466b106e847de42060cbcae1
bartoszgamza <bartosz.gamza12@gmail.com>
2018-07-26 16:13:30 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="app">
+    <div class="header"></div>
+    <div class="grid">
+      <h1>Vue Select Component</h1>
+      <p>Simple Vue selector component created with ❤️ by Bartosz Gamza</p>
+      <div class="selectorWrapper">
+        <vue-select
+        :selected="selected"
+        :options="options"
+        v-on:selectedItem="changedSelect($event)"
+        >
+        </vue-select>
+      </div>
+      <div class="selected">
+        Selected: {{selected}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import VueSelect from './components/vue-select'
+export default {
+  name: 'App',
+  data () {
+    return {
+      selected: 'Pick something...',
+      options: ['option1', 'option2', 'option3']
+    }
+  },
+  methods: {
+    changedSelect (item) {
+      this.selected = item
+    }
+  },
+  components: {
+    VueSelect
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+body {
+  padding: 0;
+}
+.header {
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(#00416A,#E4E5E6) ;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -1;
+}
+.grid {
+  max-width: 800px;
+  margin: 0 auto;
+  position: relative;
+  color: white;
+  text-align: center;
+}
+.card {
+  box-sizing: border-box;
+  z-index: 2;
+  margin-left:  auto;
+  margin-right: auto;
+  margin-top: 25px;
+  margin-bottom: 25px;
+  max-width: 800px;
+  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+  background-color: white;
+  border-radius: 5px;
+  padding: 20px 40px 5px 40px;
+}
+.selectorWrapper {
+  margin: 0 auto;
+  max-width: 400px;
+}
+.selected {
+  text-align: center;
+  padding: 10px
+}
+</style>
---
+++ src/components/vue-select.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="selector" @click="show = !show">
+    <div class="selectorInner">
+      {{selectedName}}
+    </div>
+    <div class="cardPopup">
+      <div v-if="show" class="elements" v-for="option in options" :key="option.id" @click="selector(option)">
+        {{option}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['selected', 'options'],
+  data () {
+    return {
+      show: false
+    }
+  },
+  methods: {
+    selector (user) {
+      this.selected = user
+      this.$emit('selectedItem', user)
+    }
+  },
+  computed: {
+    selectedName () {
+      const obj = this.options
+      if (obj === undefined) {
+        return 'placeholder'
+      } else {
+        return this.selected
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.selector {
+  box-sizing: border-box;
+  width: 100%;
+  height: 30px;
+  background-color: white;
+  border-radius: 15px;
+  text-align: left;
+  vertical-align: middle;
+  cursor: pointer;
+  -webkit-box-shadow: 5px 10px 10px 0px rgba(0,0,0,0.16);
+  -moz-box-shadow: 5px 10px 10px 0px rgba(0,0,0,0.16);
+  box-shadow: 5px 10px 10px 0px rgba(0,0,0,0.16);
+
+}
+.selectorInner {
+  display: inline-block;
+  vertical-align: middle;
+  margin-left: 25px;
+  margin-top: 5px;
+  text-align: left;
+  color: gray;
+  position: fixed;
+}
+.cardPopup {
+  position: relative;
+  left: 0;
+  padding-left: 25px;
+  box-sizing: border-box;
+  margin-left: auto;
+  margin-right: auto;
+  border-radius: 15px;
+  color: gray;
+  width: 100%;
+  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+  background-color: white;
+  cursor: pointer;
+}
+</style>
